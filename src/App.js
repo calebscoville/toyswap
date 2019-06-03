078221e34aba3aa48672435404452341f1b7dd45
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-03 14:15:28 -0600

    App and Index complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar'
+import {HashRouter} from 'react-router-dom'
+import router from './router'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <HashRouter>
+      <Navbar />
+      {router}
+    </HashRouter>
   );
 }
 
