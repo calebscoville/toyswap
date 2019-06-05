f88b94f9a01cc24652e7b7ac5379e1be9d4547ef
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-05 16:14:48 -0600

    Initial

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import Navbar from './Components/Navbar'
 import {HashRouter} from 'react-router-dom'
 import router from './router'
+// import 'reset-css'
 
-function App() {
+// import {
+//   AppContainer,
+//   FormContainer,
+//   FormHeader,
+//   FormTitle,
+//   FormInput,
+//   FormBtn
+// } from './AppStyles';
+
+class App extends React.Component {
+
+  render() {
   return (
     <HashRouter>
       <Navbar />
       {router}
     </HashRouter>
+
+
+
   );
+}
 }
 
 export default App;
--- src/Components/ToyInfo.js
+++ src/Components/ToyInfo.js
@@ -1,22 +1,48 @@
 import React from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
+import 'reset-css'
+// import AppStyles from './AppStyles'
 
-export const ToyInfo = () => {
+import {
+    AppContainer,
+    FormContainer,
+    FormHeader,
+    FormTitle,
+    FormInput,
+    FormBtn
+  } from './AppStyles';
+
+class ToyInfo extends React.Component {
+    render() {
     return (
-        <div>
+        
+    <AppContainer>
+  <div>
             <h1>Toy Information</h1>
-            <Button>Check Out This Toy</Button>
+            {/* <Button>Check Out This Toy</Button> */}
         </div>
+    <FormContainer>
+      <FormHeader>
+        <FormTitle>Toy Information</FormTitle>
+      </FormHeader>
+      <FormInput email="email" />
+      <FormInput />
+      <FormBtn>Check this toy out!</FormBtn>
+      <FormBtn register>Link to toy online for addtl. info</FormBtn>
+    </FormContainer>
+    </AppContainer>
+
     )
+}
 }
 
 export default ToyInfo
 
-const Button = styled.button`
-background: palevioletred;
-border-radius: 3px;
-border: 2px solid palevioletred;
-color: black;
-margin: 0 1em;
-padding: 0.25em 1em;
-`
+// const Button = styled.button`
+// background: palevioletred;
+// border-radius: 3px;
+// border: 2px solid palevioletred;
+// color: black;
+// margin: 0 1em;
+// padding: 0.25em 1em;
+// `
