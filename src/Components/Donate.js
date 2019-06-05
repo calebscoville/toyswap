a7bc742af493ee16ef5affd2cd933252cb4be6d4
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-05 11:38:10 -0600

    third

--- src/Components/Donate.js
+++ src/Components/Donate.js
@@ -1,10 +1,10 @@
 import React from 'react'
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 const Donate = () => {
     return <h1>Donate</h1>
 }
 
-
+const Button = styled.button``;
 
 export default Donate
--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components';
 
-const Test = () => {
-    return <h1>Test</h1>
-}
+export const Test = () => {
+    return (
+    <div>
+        <h1>Test</h1>
+        <Button>Submit</Button>
+    </div>
+    )
+};
+
+const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: black;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
 
 export default Test
