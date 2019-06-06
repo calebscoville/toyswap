61ce98d9f02ec581b0c4e4ecb4ebd20ea67f6af2
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-06 12:32:22 -0600

    Patron Dashboard

--- src/Components/PatronDash.js
+++ src/Components/PatronDash.js
@@ -4,11 +4,13 @@ import 'reset-css'
 import {
     AppContainer,
     FormContainer,
+    Image,
+    Image2,
     FormHeader,
     FormTitle,
     FormInput,
     FormBtn
-  } from './AppStyles2';
+  } from './AppStyles4';
 
   class PatronDash extends React.Component {
     render() {
@@ -16,17 +18,51 @@ import {
             
     <AppContainer>
       <div>
-              <h1>Toy Information</h1>
+              <h1></h1>
+              
+          </div>
+      <FormContainer>
+          <Image> Your Hot Pic Here :) </Image>
+        <FormHeader>
+          <FormTitle>Devan SSSS</FormTitle>
+        </FormHeader>
+        <text>Good day to you!</text>
+        <text>Email: devan@devmtn.com</text>
+        <text>City: Logan</text>
+        <text>Checkouts to date: 2</text>
+        <text>ID: 36</text>
+        <text>I like ping-pong, break-dancing and eating candy, especially on the weekend.</text>
+        <FormBtn register>Edit Profile</FormBtn>
+      </FormContainer>
+
+      <div>
+              <h1></h1>
               
           </div>
       <FormContainer>
         <FormHeader>
-          <FormTitle>Toy Information</FormTitle>
+          <FormTitle>What would you like to do rn?</FormTitle>
         </FormHeader>
-        <FormInput email="email" />
-        <FormInput />
-        <FormBtn>Check this toy out!</FormBtn>
-        <FormBtn register>Link to toy online for addtl. info</FormBtn>
+        <FormBtn>See All Toys</FormBtn>
+        <FormBtn register>Messages</FormBtn>
+        <FormBtn>Donate to the cause of toys!</FormBtn>
+        <FormBtn register></FormBtn>
+
+      </FormContainer>
+
+      <div>
+              <h1></h1>
+              
+          </div>
+      <FormContainer>
+        <FormHeader>
+          <FormTitle>Your History</FormTitle>
+        </FormHeader>
+        <Image2> Chutes and Ladders </Image2>
+        <FormBtn>See This Toy</FormBtn>
+        <Image2> Basic Blocks </Image2>
+        <FormBtn>See This Toy</FormBtn>
+
       </FormContainer>
   
       </AppContainer>
