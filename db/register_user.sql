d82728274522b5eb2241cbd6525bba2731a8a10a
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-07 10:06:46 -0600

    first

--- db/register_user.sql
+++ db/register_user.sql
@@ -1,4 +1,4 @@
-insert into users(image, first_name, last_name, email, username, password, city, bio, social_media)
-values (${image}, ${firstname}, ${lastname}, ${email}, ${username}, ${password}, ${city}, ${bio}, ${socialmedia})
+insert into users(first_name, last_name, email, username, password)
+values (${firstname}, ${lastname}, ${email}, ${username}, ${password})
     returning *
 ;
--- src/Components/Login/RegisterForm.js
+++ src/Components/Login/RegisterForm.js
@@ -19,16 +19,23 @@ class RegisterForm extends Component {
         axios
             .post('/auth/register', { firstname, lastname, email, username, password })
             .then((res) => {
-                this.props.history.push('/details')
+                this.props.history.push('/')
             })
             .catch((err) => {
                 console.log(err)
             })
-        e.target.firstname.value = ''
-        e.target.lastname.value = ''
-        e.target.email.value = ''
-        e.target.password.value = ''
-        e.target.username.value = ''
+        // e.target.firstname.value = ''
+        // e.target.lastname.value = ''
+        // e.target.email.value = ''
+        // e.target.password.value = ''
+        // e.target.username.value = ''
+        this.setState({
+            username: '',
+            password: '',
+            firstname: '',
+            lastname: '',
+            email: ''
+        })
     }
 
     handleRegisterInfoUpdate = (e) => {
@@ -45,33 +52,38 @@ class RegisterForm extends Component {
                         type='text'
                         placeholder='first name'
                         name='firstname'
+                        value={this.state.firstname}
                         onChange={this.handleRegisterInfoUpdate}
                     />
                     <input
                         type='text'
                         placeholder='last name'
                         name='lastname'
+                        value={this.state.lastname}
                         onChange={this.handleRegisterInfoUpdate}
                     />
                     <input
                         type='text'
                         placeholder='email'
                         name='email'
+                        value={this.state.email}
                         onChange={this.handleRegisterInfoUpdate}
                     />
                     <input
                         type='text'
                         placeholder='username'
                         name='username'
+                        value={this.state.username}
                         onChange={this.handleRegisterInfoUpdate}
                     />
                     <input
                         type='password'
                         placeholder='password'
                         name='password'
+                        value={this.state.password}
                         onChange={this.handleRegisterInfoUpdate}
                     />
-                    <button>Register</button>
+                    <button onClick={this.handleUserRegister}>Register</button>
                 </form>
             </>
         )
